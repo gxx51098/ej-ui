a5bf6d44d7f001074815c787cd623c2206f9a094
ÕÅÃÎÑÅ <756229641@qq.com>
2019-06-12 19:29:14 +0800

    实现了添加模态框的弹出和一些代码的整改。

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {message} from 'antd'
 import qs from 'qs'
 // 1. axios的默认配置
-axios.defaults.baseURL = "http://localhost:7777"
+axios.defaults.baseURL = "http://localhost:8000"
 axios.defaults.headers["Content-Type"]= "application/x-www-form-urlencoded";
 
 
@@ -18,6 +18,10 @@ axios.interceptors.response.use((response)=>{
   response.status = data.status;
   response.statusText = data.message;
   response.data = data.data;
+  if(data.status !== 200){
+    message.error(data.message)
+    return Promise.reject(response);
+  }
   return response;
 },(error)=>{
   message.error("服务端异常")
