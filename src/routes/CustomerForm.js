96deea73e8ce6aca02a38588265ed206b96583f1
ÕÅÃÎÑÅ <756229641@qq.com>
2019-06-12 19:37:16 +0800

    实现顾客信息添加的模态框和一些代码的调整

--- src/routes/CustomerForm.js
+++ src/routes/CustomerForm.js
@@ -3,7 +3,6 @@ import {Form,Modal,Input,Radio} from 'antd'
 
 class CustomerForm extends React.Component {
 
-
 render(){
     // 父组件传递给子组件值
     const { visible, onCancel, onCreate, form } = this.props;
