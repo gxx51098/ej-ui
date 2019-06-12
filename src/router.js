26d36a533e6a831191b0b07abab112f3b24753e3
gxx51098 <gxx51098@163.com>
2019-06-12 10:28:29 +0800

    ...

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import CustomerPage from './routes/CustomerPage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
+        <Route path="/" exact component={CustomerPage}/>
         <Route path="/" exact component={IndexPage} />
       </Switch>
     </Router>
